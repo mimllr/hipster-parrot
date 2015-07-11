47fb5c27cf093d119ab2ae2113d0d3323f5fe08d
Mike Miller <mike@nycda.com>
2015-07-10 22:53:38 -0400

    stupid home controller thing

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,4 @@
 class HomeController < ApplicationController
   def index
-    if current_user
-      @post = Post.new
-    else
-      flash[:alert] = "You must be logged in first."
-      redirect_to login_path
-    end
   end
 end
